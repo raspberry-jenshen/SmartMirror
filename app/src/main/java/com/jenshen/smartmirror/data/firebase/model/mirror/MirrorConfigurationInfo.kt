3f9ea4d0bd6df19340c6513e96ae1840ce26cdfc
e.samoilov <e.samoilov@olearis.com>
2016-12-07 19:21:57 +0200

    refactored mirror conf

--- app/src/main/java/com/jenshen/smartmirror/data/firebase/model/mirror/MirrorConfigurationInfo.kt
+++ app/src/main/java/com/jenshen/smartmirror/data/firebase/model/mirror/MirrorConfigurationInfo.kt
@@ -7,7 +7,7 @@ import com.jenshen.smartmirror.data.firebase.FirebaseConstant
 @IgnoreExtraProperties
 data class MirrorConfigurationInfo(@set:PropertyName(FirebaseConstant.Mirror.MirrorConfigurationInfo.TITLE)
                                    @get:PropertyName(FirebaseConstant.Mirror.MirrorConfigurationInfo.TITLE)
-                                   var id: String) {
+                                   var title: String) {
     @set:PropertyName(FirebaseConstant.MirrorConfiguration.LAST_TIME_UPDATE)
     @get:PropertyName(FirebaseConstant.MirrorConfiguration.LAST_TIME_UPDATE)
     var lastTimeUpdate: Long? = null
